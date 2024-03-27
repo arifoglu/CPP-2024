0978a00278c2942cdc845c685a065f169609c26c
arifoglu <88225121+arifoglu@users.noreply.github.com>
2024-03-27 09:54:27 +0100

    while has been done

---
+++ wschool/while/while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int input;
+    int value = 9;
+    do{
+       cout << "To find a number you enter a number : ";
+       cin >> input;
+    }while(input != value );
+    
+    return 0;
+}

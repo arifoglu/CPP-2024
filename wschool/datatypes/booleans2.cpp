ac2aa785c2d9e38ac1b45573ac9909d5c54bc79a
arifoglu <88225121+arifoglu@users.noreply.github.com>
2024-03-27 09:53:37 +0100

    datatypes  has been done

---
+++ wschool/datatypes/booleans2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int y = 6;
+    cout << (x > y ) <<endl;
+    cout << (x < y ) <<endl;
+    cout << (x == 10 ) <<endl;
+    cout << (x != 10 ) <<endl;
+    return 0;
+}

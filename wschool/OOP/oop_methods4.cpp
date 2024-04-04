9ac651f44c6ab59a9026900051965d8b63a7bddc
arifoglu <88225121+arifoglu@users.noreply.github.com>
2024-04-04 10:34:07 +0200

    oop methods have been seen

--- wschool/OOP/oop_methods4.cpp
+++ wschool/OOP/oop_methods4.cpp
@@ -5,15 +5,15 @@ using namespace std;
 class MyClass{
     public :
       int x;
-    private:
-      int y;  
+    //private:
+    //  int y;  
 };
 
 int main(){
     MyClass nums;
 
     nums.x = 25;  // public
-    nums.y = 20; // private
+    // nums.y = 20; // private
 
 
     return 0;
